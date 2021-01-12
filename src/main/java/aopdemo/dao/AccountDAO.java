27ff9259393f45d3c25e692f3c22918a10252327
tomasjakub <tomasjakub21@gmail.com>
2021-01-12 21:27:43 +0100

    Initial commit

---
+++ src/main/java/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,13 @@
+package aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+
+	public void addAccount() {
+		
+		System.out.println(getClass() + ": doing mz db work adding an account ");
+		
+	}
+}
